a6f26de4223af675ba6d0c9ec71a4d8efc57ddc1
Kaynup <punyak.dei@gmail.com>
2026-04-20 07:56:04 +0000

    chore(database): create schema of table 'bills'

---
+++ database/README.md
@@ -0,0 +1,7 @@
+# Table 'bills'
+To create table schema, run from project root: 
+
+```bash
+sudo mysql -u root -p< database/schema.sql
+```
+> enter password: 'root'
---
+++ database/schema.sql
@@ -0,0 +1,16 @@
+-- database creation and usage
+CREATE DATABASE IF NOT EXISTS family_supp_sche;
+USE family_supp_sche;
+
+-- creating 'bills' table
+CREATE TABLE bills (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    creation_date DATE NOT NULL,
+    due_date DATE NOT NULL,
+    total_amount DECIMAL(10,2) NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    category VARCHAR(50),
+
+    CHECK (due_date >= creation_date)   -- to check date integrity
+);
