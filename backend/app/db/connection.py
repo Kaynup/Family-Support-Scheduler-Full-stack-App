473fd1bf9c9a709dc2304e69dd8fcfa19059d1b4
Kaynup <punyak.dei@gmail.com>
2026-04-20 10:31:17 +0000

    chore(database/backend): setup with connection pooling

--- backend/app/db/connection.py
+++ backend/app/db/connection.py
@@ -1,12 +1,21 @@
 import mysql.connector
 from mysql.connector import pooling
+from dotenv import load_dotenv
+import os
 
-from .config import dbconfig
+load_dotenv()
+
+dbconfig = {
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+}
 
 conn_pool = pooling.MySQLConnectionPool(
     pool_name = "userpool",
-    pool_size = 5
-    **dbconfig()
+    pool_size = 5,
+    **dbconfig
 )
 
 def get_connection():
