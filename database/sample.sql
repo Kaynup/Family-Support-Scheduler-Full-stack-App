2460f236fdc057a4bd5897a1f67317c5965dbf83
Kaynup <punyak.dei@gmail.com>
2026-04-20 09:36:22 +0000

    chore(database): inserted values in sample_bills table

---
+++ database/sample.sql
@@ -0,0 +1,4 @@
+-- Generating sample data
+USE family_supp_sche;
+INSERT INTO sample_bills (name, category, total_amount, due_date, status, creation_date)
+VALUES ('Internet Bill', 'Utilities', 999, '2026-04-26', 'UNPAID', '2026-04-25');
