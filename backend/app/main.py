b2cef07357ccbe78e8a8c21d4990d393e4f2a76c
Kaynup <punyak.dei@gmail.com>
2026-04-20 10:32:19 +0000

    chore(backend): create FastAPI endpoints

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from .db.connection import get_connection
+from dotenv import load_dotenv
+import os
+
+app = FastAPI()
+
+@app.get("/health")
+def status():
+    return {"message":"backend is live"}
+
+@app.get("/db-connection-testing")
+def test_db():
+    conn = get_connection()
+    curr = conn.cursor(dictionary=True)
+    curr.execute(f"SELECT * FROM {os.getenv('TABLE')}")
+    
+    data = curr.fetchall()
+
+    curr.close()
+    conn.close()
+
+    return data
