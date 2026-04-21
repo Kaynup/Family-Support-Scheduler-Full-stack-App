7d9efeed9940a0dd99a457a179029883adc3bf18
Kaynup <punyak.dei@gmail.com>
2026-04-21 09:03:57 +0000

    chore(database/backend): create basic query functions

--- backend/app/db/queries.py
+++ backend/app/db/queries.py
@@ -8,7 +8,10 @@ load_dotenv()
 DB_C_P = int(os.getenv('DB_CONN_POOLING'))
 DB_T = os.getenv('DB_TABLE')
 
-def insert_bill(name, due_date, total_amount, creation_date=date.today(), status='UNPAID', category=None):
+def insert_bill(name, due_date, total_amount, creation_date=None, status='UNPAID', category=None):
+    if creation_date is None:
+        creation_date = date.today()
+
     conn = get_connection(DB_C_P)
     curr = conn.cursor()
 
@@ -19,11 +22,72 @@ def insert_bill(name, due_date, total_amount, creation_date=date.today(), status
     values = (name, creation_date, due_date, total_amount, status, category)
     try:
         curr.execute(query, values)
+        conn.commit()
     except mysql.connector.Error as e:
         conn.rollback()
         raise
-    else:
-        conn.commit()
+    finally:
+        curr.close()
+        conn.close()
+
+def select_all():
+    conn = get_connection(DB_C_P)
+    curr = conn.cursor()
+
+    query = f"SELECT * from {DB_T}"
+    curr.execute(query)
+
+    data = curr.fetchall()
     
     curr.close()
     conn.close()
+
+    return data
+
+def select_num_day_dues(num_days=3):
+    conn = get_connection(DB_C_P)
+    curr = conn.cursor()
+
+    query = f"""
+    SELECT * from {DB_T} WHERE status = %s
+    AND due_date BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL %s DAY)
+    ORDER BY due_date ASC
+    """
+    curr.execute(query, ('UNPAID', num_days))
+
+    data = curr.fetchall()
+    
+    curr.close()
+    conn.close()
+
+    return data
+
+def update_bill_status(id_, status):
+    conn = get_connection(DB_C_P)
+    curr = conn.cursor()
+
+    query = f"UPDATE {DB_T} SET status = %s WHERE id = %s"
+    try:
+        curr.execute(query, (status, id_))
+        conn.commit()
+    except mysql.connector.Error as e:
+        conn.rollback()
+        raise
+    finally:
+        curr.close()
+        conn.close()
+
+def delete_bill_by_id(id_):
+    conn = get_connection(DB_C_P)
+    curr = conn.cursor()
+
+    query = f"DELETE FROM {DB_T} WHERE id = %s"
+    try:
+        curr.execute(query, (id_, ))
+        conn.commit()
+    except mysql.connector.Error as e:
+        conn.rollback()
+        raise
+    finally:
+        curr.close()
+        conn.close()
